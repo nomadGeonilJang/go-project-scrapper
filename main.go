b8fcd68f57e5217242320482a5a84dc94358b3e6
nomadGeonilJang <wkdrjsdlf2@gmail.com>
2021-04-27 18:02:30 +0900

    variable expression

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func lenAndUpper(name string) (length int, uppercase string) {
-	defer fmt.Println("I'm done")
+	defer fmt.Println("I'm done") // after excute
 	length = len(name)
 	uppercase = strings.ToUpper(name)
 	return
@@ -16,9 +16,35 @@ func repeatMe(words ...string) {
 	fmt.Println(words)
 }
 
+func sayTotalNumber(total int) {
+	fmt.Println("total is ", total)
+}
+func superAdd(numbers ...int) int {
+
+	total := 0
+	//for i := 0 ; i < len(numbers) ; i++
+	for _, number := range numbers {
+		total += number
+	}
+	defer sayTotalNumber(total)
+	return total
+}
+
+func canIDrink(age int) bool {
+	if koreanAge := age + 2; koreanAge < 18 {
+		return false
+	}
+	return true
+
+}
+
 func main() {
 
-	total, upper := lenAndUpper("jgi")
-	fmt.Println(total, upper)
+	// total, upper := lenAndUpper("jgi")
+	// fmt.Println(total, upper)
+	total := superAdd(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+	fmt.Println("-->", total)
+
+	fmt.Println(canIDrink(2))
 
 }
